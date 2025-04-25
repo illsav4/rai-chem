0a0311839b8cca00e84f7a21d4742d422fcb1a7c
Illia Savchenko <168183759+illsav4@users.noreply.github.com>
2025-04-25 15:17:04 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ import setuptools
 setuptools.setup(
     name='rai-chem',
     version='0.0.4',
-    url='https://github.com/vtarasv/rai-chem.git',
-    download_url='https://github.com/vtarasv/rai-chem/archive/refs/tags/0.0.4.tar.gz',
-    install_requires=['tqdm', 'numpy', 'pandas', 'scipy', 'rdkit-pypi==2022.9.1'],
+    url='https://github.com/illsav4/rai-chem.git',
+    download_url='https://github.com/illsav4/rai-chem/archive/refs/tags/0.0.4.tar.gz',
+    install_requires=['tqdm', 'numpy', 'pandas', 'scipy', 'rdkit'],
     include_package_data=True,
     packages=setuptools.find_packages(),
     license='MIT',
